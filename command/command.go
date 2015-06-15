3b9f871ecedf3a01201ecc474f380c5de9557ade
dilhasha <dilhasha.uom@gmail.com>
2015-06-15 07:50:08 +0530

    The implementation for command related functionaliity added

---
+++ command/command.go
@@ -0,0 +1,12 @@
+package command
+
+import (
+	"github.com/cloudfoundry/cli/cf/requirements"
+	"github.com/codegangsta/cli"
+)
+
+type Command interface {
+	Metadata() CommandMetadata
+	GetRequirements(requirementsFactory requirements.Factory, context *cli.Context) (reqs []requirements.Requirement, err error)
+	Run(context *cli.Context)
+}
