d436e3c33d4b273f578deef8bb0ad56ae2c91f86
dilhasha <dilhasha.uom@gmail.com>
2015-06-23 17:21:18 +0530

    initialize command map

--- command/commandFactory.go
+++ command/commandFactory.go
@@ -1,11 +1,14 @@
 package command
 
 type concreteFactory struct {
-	cmdsByName map[string]command.Command
+	cmdsByName map[string]Command
 }
 
 func NewFactory() (factory concreteFactory) {
-	factory.cmdsByName = make(map[string]command.Command)
-	factory.cmdsByName["login"] = service.NewCreateService(ui, config, repoLocator.GetServiceRepository(), serviceBuilder)
-
+	factory.cmdsByName = make(map[string]Command)
+	//factory.cmdsByName["login"]
+	//command for triggering a build
+	//factory.cmdsByName["getAppInfo"]
+	return
 }
+
