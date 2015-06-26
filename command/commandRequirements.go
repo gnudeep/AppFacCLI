8e9445fb56bbaf17afcdfef0718d6a82b61c2d4a
dilhasha <dilhasha.uom@gmail.com>
2015-06-26 06:31:44 +0530

    requirements structure added

---
+++ command/commandRequirements.go
@@ -0,0 +1,14 @@
+package command
+
+
+
+type CommandRequirements struct {
+	UserName            string
+	Password       string
+	ApplicationKey string
+	Stage string
+	Version string
+	TagName string
+	DeployAction string
+	Cookie string
+}
