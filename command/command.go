ab6dd9bded499df9da57d46c9c33cc2ad61575f9
dilhasha <dilhasha.uom@gmail.com>
2015-07-06 08:45:24 +0530

    Run method added

--- command/command.go
+++ command/command.go
@@ -1,11 +1,10 @@
 package command
 
-
 type Command interface {
-
 	Metadata() CommandMetadata
 	Configs(reqs CommandRequirements) CommandConfigs
 	Requirements() CommandRequirements
+	Run(c CommandConfigs)
 }
 
 
